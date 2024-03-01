c426dd22a7cc77beb1def7288e4d9cb4f27a09ef
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-03-01 15:21:08 +0200

    made it better
    automated system updates and does not overwrite config files.
    
    Set sane defaults for the interval between updates to 2 weeks.

--- security-updates.sh
+++ security-updates.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 while true; do
-    # Download and install only security updates
-    apt-get update && apt-get -y -t $(lsb_release -cs)-security upgrade
+    # Download and install only security updates without changing config files
+    apt update
+    apt upgrade --without-new-pkgs --no-install-recommends -y
 
     # Schedule reboot at 00:00 the next day
     #echo "0 0 * * * reboot" | crontab -
 
-    # Sleep for 24 hours before checking for updates again
-    sleep 86400
+    # Sleep for 14 days before checking for updates again
+    sleep 1209600
 done
