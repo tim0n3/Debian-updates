18f7bb770323d0a551b75ddfcf5e37cbc6ac2596
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-03-01 13:15:49 +0200

    don't need daily reboot anymore

--- security-updates.sh
+++ security-updates.sh
@@ -5,7 +5,7 @@ while true; do
     apt-get update && apt-get -y -t $(lsb_release -cs)-security upgrade
 
     # Schedule reboot at 00:00 the next day
-    echo "0 0 * * * reboot" | crontab -
+    #echo "0 0 * * * reboot" | crontab -
 
     # Sleep for 24 hours before checking for updates again
     sleep 86400
