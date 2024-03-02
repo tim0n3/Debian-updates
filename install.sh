5b275e6290617fbb1a7278dd81346fe1e86e7559
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2024-03-02 10:38:31 +0200

    revision update
    make script a little more robust

--- install.sh
+++ install.sh
@@ -1,31 +1,60 @@
-#!/bin/bash
+#!/bin/env bash Function to log messages to stderr and a log file log() {
+	local message="$1"
+	local log_file="/var/log/$(date +"%Y-%m-%d_%T")-install-security-updates-util.log"
 
-# Check if the security-updates.sh file exists in the /root/ directory
-if [ ! -f /root/security-updates.sh ]; then
-  # Copy the security-updates.sh file from the current directory to /root/
-  cp security-updates.sh /root/
-  # Confirm that the file has been copied successfully
-  if [ $? -eq 0 ]; then
-    echo "security-updates.sh has been copied to /root/"
-  else
-    echo "Error: Failed to copy security-updates.sh to /root/"
-  fi
+	echo "$(date +"%Y-%m-%d %T"): $message" >&2
+	echo "$(date +"%Y-%m-%d %T"): $message" >> "$log_file"
+
+}
+
+# Function to handle errors
+error() {
+	echo "$(date +"%Y-%m-%d %T"): Error: $1" >&2
+	log "Error: $1" >&2
+
+	exit 1
+}
+
+# Function to copy files and handle errors
+copy() {
+	sudo cp "$1" "$2" || { 
+		error "Failed to copy $1 to $2"
+	}
+}
+
+# Function to start systemd service and handle errors
+start() {
+	sudo systemctl start "$1" || { 
+		error "Failed to start $1 service"
+	}
+	sudo systemctl enable "$1" || { 
+		error "Failed to enable $1 service"
+	}
+}
+
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+
+	log "Error: This script must be run as root."
+
+	exit 1
+
+fi
+
+# Check if security-updates.sh exists in /root/
+if [[ ! -f /root/security-updates.sh ]]; then
+	copy "security-updates.sh" "/root/"
+	echo "security-updates.sh has been copied to /root/"
 else
-  echo "security-updates.sh already exists in /root/"
+	echo "security-updates.sh already exists in /root/"
 fi
 echo "security-updates.sh lives at /root/security-updates.sh"
 
-#!/bin/bash
-
-if [ ! -f "/etc/systemd/system/myupdater.service" ]; then
-    sudo cp myupdater.service /etc/systemd/system/
-    sudo systemctl enable myupdater.service
-    sudo systemctl start myupdater.service
-    if [ $? -eq 0 ]; then
-        echo "myupdater.service installed and started successfully on $(hostname)."
-    else
-        echo "Error starting myupdater.service"
-    fi
+# Check if myupdater.service exists in systemd directory
+if [[ ! -f "/etc/systemd/system/myupdater.service" ]]; then
+	copy "myupdater.service" "/etc/systemd/system/"
+	start "myupdater.service"
+	echo "myupdater.service installed and started successfully on $(hostname)."
 else
-    echo "myupdater.service already installed on $(hostname)."
+	echo "myupdater.service already installed on $(hostname)."
 fi
